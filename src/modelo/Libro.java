0ce8ade48b3e13ee3afa4b90c3c7670c66a659a1
Marulanda6 <nahim.marulanda686@ist17dejulio.edu.ec>
2024-12-12 17:22:41 -0500

    Subida de las Clases Libro y Prestamo

---
+++ src/modelo/Libro.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author kirlok6
+ */
+public class Libro {
+    private String titulo;
+    private Autor autor;
+
+    // Constructor
+    public Libro(String titulo, Autor autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    // Getters y Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+  
+}
+
