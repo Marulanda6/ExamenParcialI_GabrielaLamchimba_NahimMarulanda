56a95e09fb127729255596f6b171178fc3a00f40
Marulanda6 <nahim.marulanda686@ist17dejulio.edu.ec>
2024-12-12 17:25:07 -0500

    persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -9,5 +9,33 @@ package modelo;
  * @author USER-LENOVO
  */
 class Persona {
+       private String nombre;
+    private String identificacion;
+
+        public Persona() {
+        }
+
+   
+    public Persona(String nombre, String identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    } 
     
 }
